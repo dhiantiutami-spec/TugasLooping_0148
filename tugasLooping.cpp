29d34115efe5a32d12c5e3f6056063b992212d21
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-03-31 20:17:55 +0700

    menambahkan return false

--- tugasLooping.cpp
+++ tugasLooping.cpp
@@ -8,3 +8,15 @@ int pilihan;
 // fungsi untuk cek bilangan prima
 bool isPrima(int x) {
     if (x <= 1) return false;
+
+    int i = 2;
+    while (i <= x / 2) {
+
+    int i = 2;
+    while (i <= x / 2)
+        if (x % i == 0) {
+            return false;
+        }
+    }
+    
+       
